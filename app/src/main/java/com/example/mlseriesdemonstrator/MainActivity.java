24231ce3b325c7f55dcf95c337b0e2d6ac5dcc84
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2023-12-19 00:45:32 -0500

    interfaz mejorada

--- app/src/main/java/com/example/mlseriesdemonstrator/MainActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/MainActivity.java
@@ -26,6 +26,9 @@ public class MainActivity extends AppCompatActivity implements AlgoListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_main);
 
         ArrayList<Algo> arrayList = new ArrayList<>();
